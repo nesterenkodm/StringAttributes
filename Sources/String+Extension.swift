4fe7eebe1bb813b14eb5746a77f5ea6f4f55bdb4
Nesterenko Dmitry <chebur.mail@gmail.com>
2018-07-24 10:52:46 +0300

    Add more String helpers

--- Sources/String+Extension.swift
+++ Sources/String+Extension.swift
@@ -10,8 +10,53 @@ import Foundation
 
 extension String {
     
-    public func with(_ attributes: StringAttributes) -> NSAttributedString {
+    /// Removes specified characters from string
+    public func replacing(charactersIn characterSet: CharacterSet, with replacement: String) -> String {
+        return components(separatedBy: characterSet).joined(separator: replacement)
+    }
+    
+    public func contains(charactersIn characterSet: CharacterSet, options: String.CompareOptions = []) -> Bool {
+        return rangeOfCharacter(from: characterSet, options: options) != nil
+    }
+    
+    public func with(_ attributes: [NSAttributedStringKey: Any]) -> NSAttributedString {
         return NSAttributedString(string: self, attributes: attributes)
     }
     
+    /// Returns the same string but with lowercased first character
+    public func lcfirst() -> String {
+        return withFirstCharacter { $0.lowercased() }
+    }
+    
+    /// Returns the same string but with uppercased first character
+    public func ucfirst() -> String {
+        return withFirstCharacter { $0.uppercased() }
+    }
+    
+    /// Apply transformation to a first character in a string
+    private func withFirstCharacter(transform: (Substring) -> (String)) -> String {
+        guard let index = self.index(startIndex, offsetBy: 1, limitedBy: endIndex) else { return self }
+        let range = startIndex..<index
+        let firstCharacter = transform(self[range])
+        return replacingCharacters(in: range, with: firstCharacter)
+    }
+    
+    public func rangesOfCharacters(from characterSet: CharacterSet, options: String.CompareOptions) -> [Range<String.Index>] {
+        var searchRange = startIndex..<endIndex
+        var ranges = [Range<String.Index>]()
+        while let range = rangeOfCharacter(from: characterSet, options: options, range: searchRange) {
+            ranges.append(range)
+            searchRange = range.upperBound..<endIndex
+        }
+        
+        return ranges.reduce(into: [Range<String.Index>]()) {
+            if let last = $0.last, last.upperBound == $1.lowerBound {
+                _ = $0.removeLast()
+                $0.append(last.lowerBound..<$1.upperBound)
+            } else {
+                $0.append($1)
+            }
+        }
+    }
+
 }
